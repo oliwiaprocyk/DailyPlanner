5439da63d1d912e34a2d97c30bb5ff92ed6450a6
Oliwia Procyk <oliwiaprocyk@gmail.com>
2023-07-29 21:21:31 +0200

    Initial Commit

---
+++ DailyPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by Oliwia Procyk on 29/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
